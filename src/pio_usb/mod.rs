7f2accf7974b93eca5c2fa421730b9aa588b1094
Raph Levien <raph@google.com>
2025-09-11 11:25:05 -0400

    Working USB loopback signaling
    This commit demonstrates working TX and RX signaling, configured as loopback. It displays the received bytes on the console.
    
    A few changes will be necessary to actually use this code. The TX and RX programs require a total of 33 instruction slots, which don't quite fit. In this commit, the final J state is omitted, and both can run at the same time, but that is not spec compliant.
    
    There are two alternate versions of TX, which do bit stuffing and NRZI, but require a 2-bit encoding of the input (a 256 entry LUT would do the trick). The code golfed one is only 8 instructions, so the juggling would not be necessary.
    
    Some notes about clocks. This code is set up for a 126 MHz sysclk, which is sensible for 480p. That will incur some jitter on output but because the ratio is a multiple of 1/256 (it is 2.625) the frequency is accurate. It may be that the USB hub regenerates a clean clock (to be validated by measurement).
    
    Clock accuracy for RX seems much less critical, as there is a low-to-high edge every 14 cycles. This RX program is tuned for 10 PIO instructions per bit but can possibly be tuned.

--- src/pio_usb/mod.rs
+++ src/pio_usb/mod.rs
@@ -1,4 +1,3 @@
-use cortex_m::peripheral::SYST;
 use embedded_hal::{
     delay::DelayNs,
     digital::{InputPin, OutputPin},
@@ -7,7 +6,7 @@ use pio::Instruction;
 use rp235x_hal::{
     gpio::{
         bank0::{Gpio1, Gpio2},
-        FunctionNull, FunctionPio0, Pin, PinState, Pins, PullDown, ValidFunction,
+        FunctionNull, Pin, PinState, Pins, PullDown, ValidFunction,
     },
     pac::{Peripherals, PIO0, RESETS},
     pio::{Buffers, PIOExt, Rx, StateMachine, Stopped, Tx, ValidStateMachine, SM0, SM1},
@@ -32,9 +31,7 @@ unsafe fn write_bitmask_clear(register: *mut u32, bits: u32) {
 #[inline]
 unsafe fn pio_sm_start<S: ValidStateMachine, State>(_sm: &StateMachine<S, State>) {
     let pio_addr = 0x50200000 + S::PIO::id() * 0x100000;
-    //write_bitmask_set(pio_addr as *mut u32, 1 << S::id());
-    // HACK: start sm0 and sm1
-    write_bitmask_set(pio_addr as *mut u32, 3);
+    write_bitmask_set(pio_addr as *mut u32, 1 << S::id());
 }
 
 #[inline]
@@ -43,13 +40,16 @@ unsafe fn pio_sm_stop<S: ValidStateMachine, State>(_sm: &StateMachine<S, State>)
     write_bitmask_clear(pio_addr as *mut u32, 1 << S::id());
 }
 
-const TX_IDLE_ADDRESS: u32 = 4;
+// TODO: set this to 4 when we reinstate J state
+const TX_IDLE_ADDRESS: u32 = 3;
 
 struct UsbPio<PIO: PIOExt> {
     tx_sm: StateMachine<(PIO, SM0), Stopped>,
     tx: Tx<(PIO, SM0)>,
     rx_sm: StateMachine<(PIO, SM1), Stopped>,
     rx: Rx<(PIO, SM1)>,
+    pio: rp235x_hal::pio::PIO<PIO>,
+    eop_irq: i32,
 }
 
 impl<PIO: PIOExt> UsbPio<PIO> {
@@ -76,18 +76,22 @@ impl<PIO: PIOExt> UsbPio<PIO> {
             .set_pins(dp.id().num, 2)
             .out_pins(dp.id().num, 2)
             .in_pin_base(dp.id().num)
-            .clock_divisor_fixed_point(1, 0)
+            // 126MHz / 48MHz
+            .clock_divisor_fixed_point(2, 160)
             .pull_threshold(8)
             .autopull(true)
             .buffers(Buffers::OnlyTx)
             .build(sm0);
         let rx_program = pio_proc::pio_file!("src/pio_usb/usb_rx.pio");
+        let eop_irq = rx_program.public_defines.IRQ_RX_EOP;
         let rx_installed = pio.install(&rx_program.program).unwrap();
         let (rx_sm, rx, _) = rp235x_hal::pio::PIOBuilder::from_installed_program(rx_installed)
             .in_pin_base(dp.id().num)
             .jmp_pin(dm.id().num)
-            .clock_divisor_fixed_point(1, 0)
-            .push_threshold(32)
+            .in_count(1)
+            // 126MHz / 120MHz
+            .clock_divisor_fixed_point(1, 13)
+            .push_threshold(8)
             .autopush(true)
             .buffers(Buffers::OnlyRx)
             .build(sm1);
@@ -96,6 +100,8 @@ impl<PIO: PIOExt> UsbPio<PIO> {
             tx,
             rx_sm,
             rx,
+            pio,
+            eop_irq,
         }
     }
 
@@ -138,7 +144,11 @@ impl<PIO: PIOExt> UsbPio<PIO> {
         });
     }
 
+    /// Transmit a 2-byte handshake packet.
+    ///
+    /// This method might go away, subsumed by `tx_packet`.
     #[link_section = ".data"]
+    #[allow(unused)]
     fn tx_handshake(&mut self, pid: u8) {
         self.setup_tx(2);
         self.tx.write(0x80);
@@ -153,6 +163,9 @@ impl<PIO: PIOExt> UsbPio<PIO> {
         }
     }
 
+    /// Transmit a packet.
+    ///
+    /// The packet must include SYNC (0x80) and any CRC.
     #[link_section = ".data"]
     fn tx_packet(&mut self, packet: &[u8]) {
         self.setup_tx(packet.len());
@@ -179,6 +192,8 @@ impl<PIO: PIOExt> UsbPio<PIO> {
         }
     }
 
+    // TODO: transmission with crc
+
     fn prime_rx(&mut self) {
         // still hacky and for debugging
         const LINE_STATE_J: u8 = 1;
@@ -200,25 +215,24 @@ impl<PIO: PIOExt> UsbPio<PIO> {
         });
 
         self.rx_sm.exec_instruction(Instruction {
-            operands: pio::InstructionOperands::WAIT {
-                polarity: 0,
-                source: pio::WaitSource::PIN,
-                index: 0,
-                relative: false,
+            operands: pio::InstructionOperands::MOV {
+                destination: pio::MovDestination::OSR,
+                op: pio::MovOperation::Invert,
+                source: pio::MovSource::NULL,
             },
             delay: 0,
             side_set: None,
         });
+        unsafe {
+            pio_sm_start(&self.rx_sm);
+        }
     }
-
-    // TODO: transmission with crc
 }
 
 #[link_section = ".data"]
 pub fn do_pio_experiment(pins: Pins, pio: PIO0, mut timer: Timer<impl TimerDevice>) {
-    let ticks0 = SYST::get_current();
-    let led = pins.gpio29.into_push_pull_output_in_state(PinState::Low);
-    let usb_host_5v_power = pins.gpio11.into_push_pull_output_in_state(PinState::High);
+    let _led = pins.gpio29.into_push_pull_output_in_state(PinState::Low);
+    let _usb_host_5v_power = pins.gpio11.into_push_pull_output_in_state(PinState::High);
     let mut usb_host_data_plus = pins.gpio1.into_push_pull_output_in_state(PinState::High);
     let mut usb_host_data_minus = pins.gpio2.into_push_pull_output_in_state(PinState::Low);
     timer.delay_ms(3);
@@ -240,28 +254,25 @@ pub fn do_pio_experiment(pins: Pins, pio: PIO0, mut timer: Timer<impl TimerDevic
     let mut usb_pio = UsbPio::new(pio, usb_host_data_plus, usb_host_data_minus, &mut resets);
 
     usb_pio.prime_rx();
-    usb_pio.tx_handshake(0xff);
+    let packet = [0x80, 0xff, 0xfe];
+    usb_pio.tx_packet(&packet);
 
     let mut a = [0u32; 16];
     let mut i = 0;
-    loop {
+    while i < a.len() {
         if let Some(d) = usb_pio.rx.read() {
-            a[i] = d;
+            a[i] = d >> 24;
             i += 1;
-            if i == a.len() {
-                break;
-            }
+        } else {
+            break;
         }
     }
-    for d in a {
-        let mut s = alloc::string::String::new();
-        for i in 0..16 {
-            let sym = (!d >> (i * 2)) & 3;
-            s.push(['0', 'J', 'K', '1'][sym as usize]);
-            if i % 4 == 3 {
-                s.push(' ');
-            }
-        }
-        console!("data: {s} {d:x}");
+    for d in &a[..i] {
+        console!("data: {d:x}");
     }
+    console!("end of data, interrupts = {:x}", usb_pio.pio.get_irq_raw());
+    unsafe {
+        pio_sm_stop(&usb_pio.rx_sm);
+    }
+    usb_pio.pio.clear_irq(1 << usb_pio.eop_irq);
 }
